2bccef06461b6a288160a1a0cc79568a720fc4c1
Rishi Ravikumar <rishiravi.k98@gmail.com>
2018-10-18 18:23:51 +0530

    Firebase authentication

--- ios/naturalDisasters/AppDelegate.m
+++ ios/naturalDisasters/AppDelegate.m
@@ -9,17 +9,18 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   NSURL *jsCodeLocation;
-
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"naturalDisasters"
+                                                      moduleName:@"NaturalDisasters"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
